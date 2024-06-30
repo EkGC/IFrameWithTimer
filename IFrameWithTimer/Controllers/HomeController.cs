f8579d5f9fbaa54b9e4e64234ee4036f06e4ebf9
EKGC <md.ekram.dayan@gmail.com>
2024-07-01 00:51:59 +0430

    Add project files.

---
+++ IFrameWithTimer/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using IFrameWithTimer.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IFrameWithTimer.Controllers
+{
+	public class HomeController : Controller
+	{
+		public TimerLinks timerLinks = new TimerLinks()
+		{
+			Id = 1,
+			Duration = 10,
+			Urls = new List<string>()
+			{
+
+                "https://www.youtube.com/embed/2ll4IA146YI",
+                "https://www.youtube.com",
+                "https://www.yahoo.com",
+                "https://www.gmail.com"
+            }
+		};
+		public IActionResult Index()
+		{
+			return View(timerLinks);
+		}
+		[HttpPost]
+		public IActionResult AddLink(string link)
+		{
+			timerLinks.Urls.Add(link);
+			return View("Index",timerLinks);
+		}
+
+		public IActionResult DeleteLink(string link)
+		{
+			timerLinks.Urls.Remove(link);
+			return View("Index",timerLinks);
+		}
+
+
+		public IActionResult SetTimer(int duration)
+		{
+			timerLinks.Duration = duration;
+			return View("Index",timerLinks);
+		}
+
+
+	}
+}
---
+++ IFrameWithTimer/Models/TimerLinks.cs
@@ -0,0 +1,9 @@
+﻿namespace IFrameWithTimer.Models
+{
+	public class TimerLinks
+	{
+        public int Id { get; set; }
+        public List<string> Urls { get; set; } = new List<string>();
+        public int Duration { get; set; }
+    }
+}
